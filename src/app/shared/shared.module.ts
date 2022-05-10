73e8745675ee5e1d4b680a56bcf16bd8fe377193
Gizelly Steffanny <gizellysteffanny@gmail.com>
2022-05-10 02:17:03 -0400

    feat: button component

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './components/button/button.component';
+import { LoaderComponent } from './components/loader/loader.component';
+
+@NgModule({
+  declarations: [
+    ButtonComponent,
+    LoaderComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ButtonComponent
+  ]
+})
+export class SharedModule { }
